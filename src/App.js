e9634f2e77cd1811fa2d7de975c6e3bada808f47
Mamen code <72928405+mamencode@users.noreply.github.com>
2021-06-10 16:47:25 +0300

    dynamic image and classname

--- src/App.js
+++ src/App.js
@@ -3,34 +3,49 @@ import "./style.css";
 
 export default function App() {
   return (
-<>
-
-
-    
-        <div class="main">
-            <div class="player player--0 player--active">
-                <h1 class="name" id="name--0" >Player 1</h1>
-                <p class="score" id="score--0">43</p> <div class="current"> <p class="current-label">Current</p> <p class="current-score" id="current--0">0</p> </div>
-            </div>
-            
-            <div class="player player--1"  >
-            <h1  class="name" id="name--1"> Player 2</h1>
-            
-            <p class="score" id="score--1">24</p> <div class="current"> <p class="current-label">Current</p> <p class="current-score" id="current--1">0</p> </div>
-                
-            </div>
-        </div>
-        <div class="dice">
-            <img src="https://i.imgur.com/HLxmLfv.png" class="diceimg" alt=""/>
-        </div>
-        <div class="footer">
-            <button  class="btn btn--new">🔄 New game</button>
-            <button class="btn btn--roll">🎲 Roll dice</button> <button class="btn btn--hold">📥 Hold</button>
+    <>
+      <div className="main">
+        <div className="player player--0 player--active">
+          <h1 className="name" id="name--0">
+            Player 1
+          </h1>
+          <p className="score" id="score--0">
+            43
+          </p>{" "}
+          <div className="current">
+            {" "}
+            <p className="current-label">Current</p>{" "}
+            <p className="current-score" id="current--0">
+              0
+            </p>{" "}
+          </div>
         </div>
 
-</>
-
-    
-    
+        <div className="player player--1">
+          <h1 className="name" id="name--1">
+            {" "}
+            Player 2
+          </h1>
+          <p className="score" id="score--1">
+            24
+          </p>{" "}
+          <div className="current">
+            {" "}
+            <p className="current-label">Current</p>{" "}
+            <p className="current-score" id="current--1">
+              0
+            </p>{" "}
+          </div>
+        </div>
+      </div>
+      <div className="dice">
+        <img src="images/dice-5.png" className="diceimg" alt="" />
+      </div>
+      <div className="footer">
+        <button className="btn btn--new">🔄 New game</button>
+        <button className="btn btn--roll">🎲 Roll dice</button>{" "}
+        <button className="btn btn--hold">📥 Hold</button>
+      </div>
+    </>
   );
 }
